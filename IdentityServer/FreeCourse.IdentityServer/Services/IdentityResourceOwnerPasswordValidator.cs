8d74d98007158adb2539fbf1974de5c3a52cc7f9
Kerim <kerimcanislamoglu1@gmail.com>
2021-05-10 21:43:49 +0300

    Finished Identity Server

---
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.IdentityServer.Models;
+using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var user = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (user == null)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors",new List<string> {"Email or Password is incorrect." });
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+            var password = await _userManager.CheckPasswordAsync(user, context.Password);
+
+            if (password == false)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string> { "Email or Password is incorrect." });
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+
+            context.Result = new GrantValidationResult(user.Id.ToString(),OidcConstants.AuthenticationMethods.Password);
+        }
+    }
+}
